d4e3caf260fa5027981ab56a401acd484d75e951
akjagadish <akshaykjagadish@gmail.com>
2024-11-06 23:05:36 +0100

    remove redundant dims and condition information in filename

--- mi/train_decisionmaking.py
+++ mi/train_decisionmaking.py
@@ -199,7 +199,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
-    env = f'{args.env_name}_dim{args.num_dims}' if args.synthetic else args.env_name if args.env_type is None else args.env_type
+    env = f'{args.env_name}' if args.synthetic else args.env_name if args.env_type is None else args.env_type
     args.ess = args.ess * args.scale + args.offset if args.job_array else args.ess
 
     # wandb configuration
@@ -233,8 +233,6 @@ if __name__ == "__main__":
     for i in range(args.runs):
 
         save_dir = f'{args.save_dir}env={env}_model={args.model_name}_num_episodes{str(args.num_episodes)}_num_hidden={str(args.num_hidden)}_lr{str(args.lr)}_num_layers={str(args.num_layers)}_d_model={str(args.d_model)}_num_head={str(args.num_head)}_noise{str(args.noise)}_shuffle{str(args.shuffle)}_paired{str(args.paired)}_loss{str(args.loss)}_ess{str(round(float(args.ess), 4))}_std{str(args.prior_std)}_run={str(args.first_run_id + i)}.pt'
-        save_dir = save_dir.replace(
-                '.pt', f'_{"ranking" if args.ranking else "direction" if args.direction else "unknown"}.pt') if args.synthetic else save_dir
         save_dir = save_dir.replace(
             '.pt', '_test.pt') if args.test else save_dir
         env_name = f'/{args.env_dir}/{args.env_name}.csv' if not args.synthetic else None
