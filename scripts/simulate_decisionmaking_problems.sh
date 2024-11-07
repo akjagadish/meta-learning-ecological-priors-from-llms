0a79fca624deb281e41b2389f67068280eb23a9c
akjagadish <akshaykjagadish@gmail.com>
2024-11-07 12:42:40 +0100

    simulate synthetic rank, direction, and unkonwn data

--- scripts/simulate_decisionmaking_problems.sh
+++ scripts/simulate_decisionmaking_problems.sh
@@ -1,18 +1,22 @@
 #!/bin/bash -l
-#SBATCH -o ./logs/%A.out
-#SBATCH -e ./logs/%A.err
-#SBATCH --job-name=SyntheticTaskGeneration  
+#SBATCH -o ./logs/%A_%a.out
+#SBATCH -e ./logs/%A_%a.err
+#SBATCH --job-name=SyntheticTaskGeneration
+#SBATCH --time=24:00:00
+#SBATCH --mem=40G
+#SBATCH --cpus-per-task=72
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=akshaykjagadish@gmail.com
-#SBATCH --time=72:00:00
-#SBATCH --cpus-per-task=18
 
 
-cd ~/ermi/mi/
+cd ~/ermi/
 
 module purge
 module load anaconda/3/2023.03
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 
 
-python simulate_data.py --num-tasks 10000 --num-dims 2 --max-steps 20 --paradigm decisionmaking
+# python mi/simulate_data.py --num-tasks 10000 --num-dims 2 --max-steps 20 --paradigm decisionmaking
+python mi/simulate_data.py --num-tasks 10000 --num-dims 4 --max-steps 20 --paradigm decisionmaking
+python mi/simulate_data.py --num-tasks 400 --num-dims 4 --max-steps 20 --paradigm decisionmaking --ranked
+python mi/simulate_data.py --num-tasks 400 --num-dims 4 --max-steps 20 --paradigm decisionmaking --direction
